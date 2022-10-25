68624afa771c4cdc3750db1af4005e063bfb911f
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2022-10-26 02:50:55 +0300

    Create 1662.CheckIfTwoStringArraysareEquivalent.java

---
+++ LeetCode/1662.CheckIfTwoStringArraysareEquivalent.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        
+        """
+        Mission
+        Given two string arrays word1 and word2, return true if the two arrays represent the same string, 
+        and false otherwise.
+        A string is represented by an array if the array elements concatenated in order forms the string.
+        """
+        
+        return String.join("", word1).equals(String.join("", word2));
+    }
+}

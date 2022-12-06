7790f7f1b3a4c6ac02df94e3aa7fcb054c490113
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2022-12-06 11:23:09 +0300

    variable program

---
+++ C#/Variables/Program.cs
@@ -0,0 +1,92 @@
+﻿// Hello World! program
+using System;
+using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            long mobileNum;
+
+            // long long;
+
+            int @void;
+            int @break;
+            int @continue;
+
+            int number;
+            int calculateMarks;
+
+            // string hello$;
+            // int 1name;
+
+            string name1;
+            bool @if;
+
+            // bool if;
+            // string My Name;
+
+            string _hello_hi;
+
+            mobileNum = 0;
+            @void = 345;
+            @void = 300;
+
+            int age;
+            // float age;
+
+            var value = 5;
+
+            int value1;
+            value = 5;
+
+            // var value2;
+
+            bool isValid = true;
+            Console.WriteLine("Bool (true, false): " + isValid);
+
+            sbyte level = 23;
+            Console.WriteLine("sbyte (8 bits both): " + level);
+
+            short num1 = -1109;
+            Console.WriteLine("short (16 bits both): " + num1);
+
+            int num2 = 51092;
+            Console.WriteLine("int (32 bits both): " + num2);
+
+            long num3 = -709183231L;
+            Console.WriteLine("long (64 bits both): " + num3);
+
+            byte num4 = 129;
+            Console.WriteLine("byte (8 bits positive): " + num4);
+
+            ushort num5 = 43543;
+            Console.WriteLine("ushort (16 bits positive): " + num5);
+
+            uint num6 = 1142345;
+            Console.WriteLine("uint (32 bits positive): " + num6);
+
+            ulong num7 = 17091821871L;
+            Console.WriteLine("ulong (64 bits positive): " + num7);
+
+            float num8 = 43.27F;
+            Console.WriteLine("float (32 bits both): " + num8);
+
+            double num9 = -11092.53D;
+            Console.WriteLine("double (64 bits both): " + num9);
+
+            char ch1 = '\u0042';
+            char ch2 = 'x';
+            Console.WriteLine("char (16 bits): " + ch1 + "," + ch2);
+
+            decimal num10 = 53005.25M;
+            Console.WriteLine("decimal (128 bits both): " + num10);
+
+            Console.WriteLine("Also there are another chars");
+            Console.WriteLine("\' , \" , \\ , abc , \n , abc , \r , abc , \t , abc , \a , abc , \b , abc");
+
+        }
+    }
+}

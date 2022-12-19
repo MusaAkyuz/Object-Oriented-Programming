a12b5a4889b8c6a0e8474a3e8f25e465361351e8
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2022-12-20 00:29:54 +0300

    update

--- C#/ReceiptPrinter/Form1.cs
+++ C#/ReceiptPrinter/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Data.OleDb;
 using Microsoft.VisualBasic.ApplicationServices;
 using System.IO;
+//using System.Data.SqlClient;
 
 namespace ReceiptPrinter
 {
@@ -23,11 +24,14 @@ namespace ReceiptPrinter
 		private void Form1_Load(object sender, EventArgs e)
 		{
 			//Connection settings
-			string cnnString = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = C:\\Users\\muss\\Desktop\\Database\\Database1.accdb";
+			string cnnString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\muss\\Desktop\\Database\\Database1.accdb";
+			//string cnnString2 = "Data Source=.;Initial Catalog=Customer;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 			//OleDbConnection cnn = new OleDbConnection(cnnString);
 			OleDbConnection cnn = new OleDbConnection(cnnString);
+			//SqlConnection cnn = new SqlConnection(cnnString2);
 			cnn.Open();
 			OleDbDataAdapter _dataAdapter = new OleDbDataAdapter("SELECT * FROM Stock", cnnString);
+			//SqlDataAdapter _dataAdapter = new SqlDataAdapter("SELECT * FROM Customer", cnnString2);
 			DataSet _dataSet = new DataSet();
 			_dataAdapter.Fill(_dataSet, "Stock");
 			databaseView.DataSource = _dataSet.Tables["Stock"];

701a383e70387acdd8b7e23bdc12207c20888acb
MusaAkyuz <musakyuz0@gmail.com>
2023-03-21 08:21:59 +0300

    Execution powershell script via c#

---
+++ C#/ExecutePowerShellScript/ExecutePowerShellScript/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ExecutePowerShellScript
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            InstallViaPowerShell();
+        }
+
+        public static void InstallViaPowerShell()
+        {
+
+            var ps1File = @"./display.ps1";
+
+            var startInfo = new ProcessStartInfo()
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -ExecutionPolicy ByPass -File \"{ps1File}\"",
+                UseShellExecute = false
+            };
+            Process.Start(startInfo);
+
+        }
+    }
+}
